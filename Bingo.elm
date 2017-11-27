ad919cdc89d33deb03c76adf7766dda32d296a7b
Peter Tillemans <pti@melexis.com>
2017-11-27 22:59:41 +0100

    add elm to html page

--- Bingo.elm
+++ Bingo.elm
@@ -1,7 +1,44 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+playerInfo name gameNumber =
+    name ++ " - game #" ++ (toString gameNumber)
+
+
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2
+            [ id "info", class "classy" ]
+            [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org" ]
+            [ text "Powered by Elm." ]
+        ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "Mike" 3
+        , viewFooter
+        ]
 
 
 main =
-    Html.text "Hello, Elm!"
+    view
